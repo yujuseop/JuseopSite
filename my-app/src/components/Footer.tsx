10539301604a0daa97f8605d7ab255c302b26f94
유주섭 <noname@nonameui-MacBookAir.local>
2025-11-01 22:22:50 +0900

    fix:Footer

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="flex justify-center items-center p-6 bg-gray-200 dark:bg-gray-800 text-black dark:text-white transition-colors duration-200">
-      <p>&copy; {new Date().getFullYear()} Welcome toJuseop World!</p>
+      <p>&copy; {new Date().getFullYear()} JS World!</p>
     </footer>
   );
 }
