9d9f3bbfa9045b1f685de7dcbde5ea0b7fb600eb
유주섭 <noname@nonameui-MacBookAir.local>
2025-12-03 15:18:23 +0900

    fix:Foorter내용 수정

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="flex justify-center items-center p-6 bg-gray-200 dark:bg-gray-800 text-black dark:text-white transition-colors duration-200">
       <p className="text-center">
         &copy; {new Date().getFullYear()} JS World! <br /> 궁금하신 점이
-        있으시면 언제든 연락주세요.
+        있으시면 언제든 연락주세요!
       </p>
     </footer>
   );
